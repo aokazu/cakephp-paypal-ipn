2807b0a6e038fa76c330e54453bbb8ffd00dc289
Dominik Schmelz <dominik.schmelz@otoso.net>
2016-10-02 19:31:32 +0200

    Remove utf8 and unicode from the migration

--- tests/Fixture/InstantPaymentNotificationsFixture.php
+++ tests/Fixture/InstantPaymentNotificationsFixture.php
@@ -105,11 +105,11 @@ class InstantPaymentNotificationsFixture extends TestFixture
 		'_constraints' => [
 			'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
 		],
-		'_options' => [
-			'engine' => 'InnoDB',
-			'charset' => 'utf8',
-			'collation' => 'utf8_unicode_ci'
-		],
+//		'_options' => [
+//			'engine' => 'InnoDB',
+//			'charset' => 'utf8',
+//			'collation' => 'utf8_unicode_ci'
+//		],
 	];
 
 	/**
